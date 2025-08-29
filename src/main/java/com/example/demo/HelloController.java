4b716f02e8990cf45f1be83527ac11b20bb6c833
cjhamby <cjhamby@protonmail.com>
2025-08-29 19:06:49 -0400

    cool message

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,12 +1,17 @@
 package com.example.demo;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 
-@RestController
+@Controller
+@RequestMapping("/hello")
 public class HelloController {
-    @GetMapping("/")
+    
+    @GetMapping
+    @ResponseBody
     public String index() {
         return "hello world!";
     }
